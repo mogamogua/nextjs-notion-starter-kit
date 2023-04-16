a783c1237f164f205113ed3ff0f6dee60bea605c
Jisu Kim <jskim@elicer.com>
2023-04-16 14:13:11 +0900

    style(notion.css): change into scss format, adjust gallery card style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,11 @@ import posthog from 'posthog-js'
 import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
-import 'styles/global.css'
+import 'styles/global.scss'
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
-import 'styles/notion.css'
+import 'styles/notion.scss'
 // global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
 
