08642f98f4ca4f2cbb693c67f46f6144eb443036
Jisu Kim <jskim@elicer.com>
2023-04-16 17:10:50 +0900

    config(site.config): add portfolio page, linkedin id

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Jisu Kim',
 
   // open graph metadata (optional)
-  description: 'Tech Blog mainly cover Web development category',
+  description: ' Journey of Learning about Web Programming',
 
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
@@ -46,16 +46,13 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Portfolio',
+      pageId: '876698185ebf4947864e345bec605e35'
+    }
+  ],
+  linkedin: 'b80abb231'
 })
